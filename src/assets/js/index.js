e7fa5a1984c8dfea618d84d657df633406088ad8
Somes20 <141081060+Somes20@users.noreply.github.com>
2023-08-08 02:51:34 +0400

    Mise à jours

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -22,9 +22,8 @@ class Splash {
 
     async startAnimation() {
         let splashes = [
-            { "message": "Je... vie...", "author": "Luuxis" },
-            { "message": "Salut je suis du code.", "author": "Luuxis" },
-            { "message": "Linux n' ai pas un os, mais un kernel.", "author": "Luuxis" }
+            { "message": "THRUST YOURSELF", "author": "GoldenJet" },
+            { "message": "Le plaisir à l'état brut", "author": "Somes" }
         ]
         let splash = splashes[Math.floor(Math.random() * splashes.length)];
         this.splashMessage.textContent = splash.message;
